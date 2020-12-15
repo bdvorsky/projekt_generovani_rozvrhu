684094654aeb02a1868093ccc93c10a266c76174
Michaela Novotna <michaela.novotna.8@seznam.cz>
2020-12-15 21:21:39 +0100

    Úprava tříd

--- modul_tridy.py
+++ modul_tridy.py
@@ -3,7 +3,7 @@ class Clovek:
         self.name = name
         self.course = course
         self.mozne_hodiny = mozne_hodiny
-    
+
     def __str__(self):
         return self.name
     
@@ -21,11 +21,24 @@ class Clovek:
 
 
 class Student(Clovek):
-    def pridej_studenta(self, name, course, mozne_hodiny):
-        student = Student(name, course, mozne_hodiny)
-        self.studenti.append(student)
+    
+    def __init__(self, name, course, mozne_hodiny, cas_kurzu):
+        Clovek.__init__(self, name, course, mozne_hodiny)
+        self.cas_kurzu = cas_kurzu
+    
+    def vrat_cas_kurzu(self):
+        return self.cas_kurzu
+
+    # def pridej_studenta(self, name, course, mozne_hodiny):
+        # student = Student(name, course, mozne_hodiny)
+        # self.studenti.append(student)
 
 
 class Lektor(Clovek):
+    
+    def __init__(self, name, course, mozne_hodiny, schedule):
+        Clovek.__init__(self, name, course, mozne_hodiny)
+        self.schedule = schedule
+
     def vrat_rozvrh(self):
         return self.schedule
